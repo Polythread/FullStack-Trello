f68230395e4d4ad2c98c24f82a578acb23e84b12
Mantecmac-10 <Mantech468@gmail.com>
2026-08-17 01:03:25 +0530

    zod_schema trello

--- packages/common/index.ts
+++ packages/common/index.ts
@@ -4,3 +4,32 @@ export const signupSchema = z.object({
   email: z.email(),
   password: z.string().min(6),
 });
+
+export const loginSchema = signupSchema.pick({
+  email: true,
+  password: true,
+});
+
+export const orgSchema = z.object({
+  name: z.string().nonempty(),
+  description: z.string(),
+});
+
+export const boardSchema = z.object({
+  title: z.string(),
+  description: z.string(),
+});
+
+export const sectionSchema = z.object({
+  title: z.string(),
+});
+
+export const issueSchema = z.object({
+  title: z.string(),
+  description: z.string(),
+});
+
+export type Organization = z.infer<typeof orgSchema>;
+export type Board = z.infer<typeof boardSchema>;
+export type Section = z.infer<typeof sectionSchema>;
+export type Issue = z.infer<typeof issueSchema>;
