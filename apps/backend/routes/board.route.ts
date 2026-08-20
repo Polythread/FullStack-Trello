570ce491f793cc9e16a71581b99deb28fcfeb970
Mantecmac-10 <Mantech468@gmail.com>
2026-08-20 23:51:45 +0530

    board endpoint updated, Trello

--- apps/backend/routes/board.route.ts
+++ apps/backend/routes/board.route.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { verifyUser } from "../middlewares/auth.middleware";
-import { createBoard } from "../controllers/board.controller";
+import { createBoard, getBoards } from "../controllers/board.controller";
 
 const router = Router();
 
 router.post("/:orgId", verifyUser, createBoard);
+router.get("/:orgId", verifyUser, getBoards);
 
 export default router;
